d083427af9cbc7cf2482d7e7274682b8213b5824
ktulhu2k <ktulhu2k@gmail.com>
2025-09-03 16:28:50 +0300

    Create hw1.py

---
+++ src/hw1.py
@@ -0,0 +1,12 @@
+from customer_system import Customer, Order, print_customer_report
+
+c1 = Customer(1,"SAP Customer")
+o1 = Order(101,500)
+o2 = Order(102,800)
+c1.add_order(o1)
+c1.add_order(o2)
+
+print_customer_report(c1)
+
+c2=Customer(2,"Empty Customer")
+print_customer_report(c2) 
